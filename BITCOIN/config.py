9fcb9ce6b96f8b4a446420bd3a0e5f9d12cb60fb
adharidwan <adharidwan2115@gmail.com>
2025-08-27 18:29:50 +0700

    fix: path

--- BITCOIN/config.py
+++ BITCOIN/config.py
@@ -7,7 +7,7 @@ from datetime import datetime
 TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 GENESIS_TIMESTAMP = datetime.strptime(datetime.now().strftime(TIMESTAMP_FORMAT), TIMESTAMP_FORMAT)
 
-with open("SeleksiSisterB2/BITCOIN/.env", "w") as file:
+with open("./.env", "w") as file:
     file.write(f"DEFAULT_DIFFICULTY={DEFAULT_DIFFICULTY}\n")
     file.write(f"DEFAULT_PORT={DEFAULT_PORT}\n")
     file.write(f"SYNC_INTERVAL={SYNC_INTERVAL}\n")
